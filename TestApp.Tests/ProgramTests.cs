5ad3afa0e0e689c3045a301b0e8bc6f9e0512b5b
Magnus Brun Larsen <magnusbrunlarsen00@gmail.com>
2022-09-02 13:17:20 +0200

    Init

---
+++ TestApp.Tests/ProgramTests.cs
@@ -0,0 +1,12 @@
+namespace TestApp.Tests;
+
+using Xunit;
+public class ProgramTests
+{
+    [Fact]
+    public void TestHelloWorld()
+    {
+        var program = new Program();
+        program.testThis(arg);    
+    }
+}
---
+++ TestApp/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+internal class Program
+{
+    public static string testThis(string arg) 
+    {
+        var program = new Program();
+        var result = program("test");
+    }
+}
