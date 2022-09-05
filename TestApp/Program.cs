6345303ef5572b39112bfe35f5eb7984c2e77c98
Magnus Brun Larsen <magnusbrunlarsen00@gmail.com>
2022-09-05 10:16:07 +0200

    Implement checking for contennial years

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -11,6 +11,14 @@ public class Program
     {
         if(year % 4 == 0) 
         {
+            if(year % 100 == 0)
+            {
+                if(year % 400 == 0)
+                {
+                    return true;
+                }
+                return false;
+            }
             return true;
         }
         return false;
