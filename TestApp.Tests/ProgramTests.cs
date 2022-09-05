bee6537124bef2fff8ade0b9134490f6d72c7aa9
Magnus Brun Larsen <84317634+magnusblarsen@users.noreply.github.com>
2022-09-05 10:18:55 +0200

    Tested and implemented IsLeapYear (#2)
    * Add tests for isLeapYear, that checks if it can divide numbers by 4
    
    * Implement solution to tests and fix one test"
    
    * Add tests checking for contennial years
    
    * Implement checking for contennial years

--- TestApp.Tests/ProgramTests.cs
+++ TestApp.Tests/ProgramTests.cs
@@ -17,4 +17,66 @@ public class ProgramTests
         var output = writer.GetStringBuilder().ToString().TrimEnd();
         output.Should().Be("Hello World!");
     }
+
+
+    [Fact]
+    public void DivideBy4True()
+    {
+        // Arrange
+        using var writer = new StringWriter();
+        Console.SetOut(writer);
+
+         // Act
+        var program = Assembly.Load(nameof(TestApp));
+        bool result2004 = Program.isLeapYear(2004);
+        bool result1512 = Program.isLeapYear(1512);
+        bool result44 = Program.isLeapYear(44);
+        
+
+        // Assert
+        result2004.Should().BeTrue();
+        result1512.Should().BeTrue();
+        result44.Should().BeTrue();
+    }
+
+
+    [Fact]
+    public void DivideBy4False()
+    {
+        // Arrange
+        using var writer = new StringWriter();
+        Console.SetOut(writer);
+
+         // Act
+        var program = Assembly.Load(nameof(TestApp));
+        bool result2006 = Program.isLeapYear(2006);
+        bool result1511 = Program.isLeapYear(1511);
+        bool result47 = Program.isLeapYear(47);
+        
+
+        // Assert
+        result2006.Should().BeFalse();
+        result1511.Should().BeFalse();
+        result47.Should().BeFalse();
+    }
+
+
+    [Fact]
+    public void TestEdgeCaseDivideBy100()
+    {
+        // Arrange
+        using var writer = new StringWriter();
+        Console.SetOut(writer);
+
+         // Act
+        var program = Assembly.Load(nameof(TestApp));
+        bool result1300 = Program.isLeapYear(1300);
+        bool result1600 = Program.isLeapYear(1600);
+        
+
+        // Assert
+        result1300.Should().BeFalse();
+        result1600.Should().BeTrue();
+    }
+    
 }
--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -6,4 +6,21 @@ public class Program
     {
         Console.WriteLine("Hello World!");
     }
+
+    public static bool isLeapYear(int year) 
+    {
+        if(year % 4 == 0) 
+        {
+            if(year % 100 == 0)
+            {
+                if(year % 400 == 0)
+                {
+                    return true;
+                }
+                return false;
+            }
+            return true;
+        }
+        return false;
+    }
 }
