aaf652f601b466fecde60c4771ab69017244543f
Magnus Brun Larsen <84317634+magnusblarsen@users.noreply.github.com>
2022-09-06 08:45:50 +0200

    Implement exception-handling (#3)
    * Add tests for exception-handling
    
    * Add exception-handling

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -4,7 +4,22 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        string result = isLeapYear(getInput()) ? "yay" : "nay";
+        string result;
+        try
+        {
+            result = isLeapYear(getInput()) ? "yay" : "nay";
+        } 
+        catch(ArgumentException e) 
+        {
+            Console.WriteLine(e.Message);
+            return;
+        } 
+        catch(FormatException)
+        {
+            Console.WriteLine("Something went wrong: You must write a number");
+            return;
+        }
+
         Console.WriteLine(result);
     }
 
@@ -13,12 +28,19 @@ public class Program
         string? line = Console.ReadLine();
         if (line != null)
         {
-            return int.Parse(line);
+            int result = int.Parse(line);
+            return result;
         }
         return -1;
     }
     public static bool isLeapYear(int year)
     {
+        if (year < 1582)
+        {
+            throw new ArgumentException("Can't calculate for years before 1582");
+        }
+
+
         if (year % 4 == 0)
         {
             if (year % 100 == 0)
